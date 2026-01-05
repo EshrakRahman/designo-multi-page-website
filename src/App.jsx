64c79c2be4d86ef12bbf98941d6bbeeb1c81815c
Eshrak Rahman <eshrakurrahman@gmail.com>
2026-01-06 04:07:52 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-red-500">hello</h1>
+      <div className="bg-light-grey p-8">
+        <h1>Hello Designo</h1>
+        <h2>Subheading Example</h2>
+        <h3>Section Title</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium.
+        </p>
+        <button className="primary">Primary Button</button>
+        <button className="secondary ml-4">Secondary Button</button>
+      </div>
+    </>
+  );
+}
---
+++ src/assets/index.js
@@ -0,0 +1,64 @@
+// About Page
+export { default as imageAboutHeroDesktop } from "./about/desktop/image-about-hero.jpg";
+export { default as imageAboutHeroMobile } from "./about/mobile/image-about-hero.jpg";
+export { default as imageAboutHeroTablet } from "./about/tablet/image-about-hero.jpg";
+
+export { default as imageRealDealDesktop } from "./about/desktop/image-real-deal.jpg";
+export { default as imageWorldClassTalentDesktop } from "./about/desktop/image-world-class-talent.jpg";
+
+// App Design
+export { default as imageAirfilter } from "./app-design/desktop/image-airfilter.jpg";
+export { default as imageEyecam } from "./app-design/desktop/image-eyecam.jpg";
+export { default as imageFaceit } from "./app-design/desktop/image-faceit.jpg";
+export { default as imageLoopstudios } from "./app-design/desktop/image-loopstudios.jpg";
+export { default as imageTodo } from "./app-design/desktop/image-todo.jpg";
+
+// Contact
+export { default as bgPatternHeroContactDesktop } from "./contact/desktop/bg-pattern-hero-desktop.svg";
+export { default as bgPatternHeroContactMobile } from "./contact/mobile/bg-pattern-hero-contact-mobile.svg";
+export { default as iconError } from "./contact/desktop/icon-error.svg";
+
+// Graphic Design
+export { default as imageBoxedWater } from "./graphic-design/desktop/image-boxed-water.jpg";
+export { default as imageChange } from "./graphic-design/desktop/image-change.jpg";
+export { default as imageScience } from "./graphic-design/desktop/image-science.jpg";
+
+// Home Page
+export { default as bgPatternHeroHomeDesktop } from "./home/desktop/bg-pattern-hero-home.svg";
+export { default as illustrationPassionate } from "./home/desktop/illustration-passionate.svg";
+export { default as illustrationFriendly } from "./home/desktop/illustration-friendly.svg";
+export { default as illustrationResourceful } from "./home/desktop/illustration-resourceful.svg";
+export { default as imageAppDesignHomeDesktop } from "./home/desktop/image-app-design.jpg";
+export { default as imageGraphicDesignHomeDesktop } from "./home/desktop/image-graphic-design.jpg";
+export { default as imageWebDesignHomeDesktop } from "./home/desktop/image-web-design-large.jpg";
+export { default as imageWebDesignHomeSmall } from "./home/desktop/image-web-design-small.jpg";
+export { default as imageHeroPhone } from "./home/desktop/image-hero-phone.png";
+
+// Locations
+export { default as imageMapAustralia } from "./locations/desktop/image-map-australia.png";
+export { default as imageMapCanada } from "./locations/desktop/image-map-canada.png";
+export { default as imageMapUK } from "./locations/desktop/image-map-united-kingdom.png";
+
+// Shared Assets
+export { default as bgPatternLeaf } from "./shared/desktop/bg-pattern-leaf.svg";
+export { default as bgPatternCallToAction } from "./shared/desktop/bg-pattern-call-to-action.svg";
+export { default as bgPatternTwoCircles } from "./shared/desktop/bg-pattern-two-circles.svg";
+export { default as bgPatternSmallCircle } from "./shared/desktop/bg-pattern-small-circle.svg";
+export { default as bgPatternThreeCircles } from "./shared/desktop/bg-pattern-three-circles.svg";
+
+export { default as iconFacebook } from "./shared/desktop/icon-facebook.svg";
+export { default as iconInstagram } from "./shared/desktop/icon-instagram.svg";
+export { default as iconTwitter } from "./shared/desktop/icon-twitter.svg";
+export { default as iconYoutube } from "./shared/desktop/icon-youtube.svg";
+export { default as iconPinterest } from "./shared/desktop/icon-pinterest.svg";
+export { default as iconRightArrow } from "./shared/desktop/icon-right-arrow.svg";
+
+export { default as illustrationCanada } from "./shared/desktop/illustration-canada.svg";
+export { default as illustrationUK } from "./shared/desktop/illustration-united-kingdom.svg";
+export { default as illustrationAustralia } from "./shared/desktop/illustration-australia.svg";
+
+export { default as logoDark } from "./shared/desktop/logo-dark.png";
+export { default as logoLight } from "./shared/desktop/logo-light.png";
+
+// Favicon
+export { default as favicon32 } from "./favicon-32x32.png";
